c548288262e8f5b4e03d256d7aa6170a93ca9ff0
SkyForce <nikestrike0@gmail.com>
2017-02-13 04:58:18 +0700

    if breaks added

--- Debugging/RobotsLanguageReport.js
+++ Debugging/RobotsLanguageReport.js
@@ -1,17 +1,15 @@
 ﻿
 function SubprogramNode1() {
+    return;
 }
-for (IterationsNode1 = 0; IterationsNode1 < 11; IterationsNode1++) {
-    if (x > 5) {
-        for (IterationsNode2 = 0; IterationsNode2 < 10; IterationsNode2++) {
-            while(true) {
-                SubprogramNode1();
-                SubprogramNode1();
-            }
-        }
+if (condition) {
+    if (condition) {
+        return;
     }
     else {
-        SubprogramNode1();
     }
 }
+else {
+}
+return;
 
